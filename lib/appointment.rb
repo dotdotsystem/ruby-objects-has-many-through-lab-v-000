6d3d0fe320469830cb00695f09fb58469fe2dda3
Claire Harmer <claire.f.harmer@gmail.com>
2018-09-13 01:16:22 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,16 +1,16 @@
-class Appointment #belongs to patients and doctors
-   attr_accessor :date, :patient, :doctor
-   @@all = []
+class Appointment
 
-   def initialize(date, patient, doctor)
-     @patient = patient
-     @date = date
-     @doctor = doctor
-     @@all << self
-   end
+  attr_accessor :date, :patient, :doctor
+  @@all = []
 
-   def self.all
-     @@all
-   end
+  def initialize(patient, doctor, date)
+    @patient = patient
+    @doctor = doctor
+    @date = date
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
 end
