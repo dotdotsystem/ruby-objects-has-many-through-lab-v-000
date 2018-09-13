f71d7348ee6b126b4f24679186fc17bdc4940a12
Claire Harmer <claire.f.harmer@gmail.com>
2018-09-13 00:22:37 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,28 +1,28 @@
-class Artist  
+class Artist
+
   @@all = []
 
-  def self.all 
+  def self.all
     @@all
   end
 
   def initialize (name)
-    @name = name 
-    @@all < self  
+    @name = name
+    @@all < self
   end
 
-def new_song(name, artist)
-  new_song.new(name, artist)
-end 
+def new_song(name, genre)
+  new_song.new(name, genre)
+end
 
-def songs 
+def songs
   Arist.all.select do |song|
     Artist.song
-end 
+end
 
 def genres
   Artist.map do |genre|
     genre.songs
   end
 
-end 
-  
+end
